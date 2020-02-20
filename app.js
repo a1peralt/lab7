38f41c0d5c6e989263d1d92d005e295385f3af80
Albert Peralta <a1peralt@ucsd.edu>
2020-02-20 00:14:05 -0800

    Added viewAlt

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', index.viewAlt);
+app.get('/', index.view);
 app.get('/viewAlt', index.viewAlt);
 app.get('/project/:id', project.view);
 // Example route
